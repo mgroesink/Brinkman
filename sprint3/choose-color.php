4b241b5d5e57e1a750888abab4e0b7e42fbf1f5f
mgroesink <mroesink@rocvantwente.nl>
2018-02-19 07:39:17 +0100

    choose-color toegevoegd

---
+++ sprint3/choose-color.php
@@ -0,0 +1,142 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Choosecolors</title>
+</head>
+
+
+<?php
+/**
+ *CreatedbyPhpStorm.
+ *User:Gebruiker
+ *Date:18-2-2018
+ *Time:15:23
+ */
+
+if (isset($_POST["submit"])) {
+
+    echo "<bodystyle='background-color:" . $_POST["backcolor"]
+        . ";color:" . $_POST["forecolor"] . "'>";
+} else {
+    echo "<body>";
+}
+
+?>
+<form action="" method="post">
+    <select name
+            ="backcolor">
+        <option value
+                ="red">Red
+        </option>
+        <option value
+                ="blue">Blue
+        </option>
+        <option value
+                ="black">Black
+        </option>
+        <option value
+                ="green">Green
+        </option>
+        <option value
+                ="yellow">Yellow
+        </option>
+        <option value
+                ="brown">Brown
+        </option>
+        <option value
+                ="cyan">Cyan
+        </option>
+        <option value
+                ="magenta">Magenta
+        </option>
+        <option value
+                ="orange">Orange
+        </option>
+    </select>
+    <select name
+            ="forecolor">
+        <option value
+                ="red">Red
+        </option>
+        <option value
+                ="blue">Blue
+        </option>
+        <option value
+                ="black">Black
+        </option>
+        <option value
+                ="green">Green
+        </option>
+        <option value
+                ="yellow">Yellow
+        </option>
+        <option value
+                ="brown">Brown
+        </option>
+        <option value
+                ="cyan">Cyan
+        </option>
+        <option value
+                ="magenta">Magenta
+        </option>
+        <option value
+                ="orange">Orange
+        </option>
+    </select>
+    <inputtype
+    ="submit"name="submit">
+</form>
+<br>
+<imgsrc
+="images/cicero2.jpg"width="150"alt="LoremIpsum"/>
+<p>Loremipsumdolorsitametconsectetur,adipiscingelitnequelaoreetnisleleifend,egetduisodioultricies.Nisl
+    gravidatemporsemfacilisicuraemorbisollicitudinhabitassemagnaluctus,nibhodiotorquentvariusscelerisque
+    nuncvenenatispernam.Temporlobortisnetusblanditplaceratnibhtinciduntcuraetristiquedisfamesfacilisi
+    hac,laoreetvelitullamcorperegestasveltellusidhendreritsemperridiculus.Maurishimenaeossodalesiaculis
+    sapieninterdumvenenatis,quisportapenatibussuspendisseconguea,dictumstfacilisisconsequatnullatortor.
+    Nibhaccumsanarcuhimenaeosvelitdiampenatibuslacinia,sagittiscondimentumtellusmagnaconsequatindonec,
+    ligulamirisusodioaliquetultrices.Erossodalesplaceratleoanteduisnatoquefringillalectusnibhest,
+    scelerisqueconguesapienphasellusinterdumnetusauctorarcuaptentneccurabitur,pharetravenenatismassa
+    ridiculusquisquelaoreetjustoornaretempus.Sagittisvivamusclasscursuspraesentconubiasociosqu,suspendisse
+    arcuauctorhactempustellus,velitcrassenectusmattistorquent.</p>
+<p>Aliquameroscondimentumquisquecongueplaceratadegestasclassmattis,rutrumtinciduntnostraparturient
+    himenaeostorquentnamtristique,mollisnequefaucibusfamespotenticonsequatlitoraorci.Tellussuscipit
+    faucibusanteacondimentumaptentin,massaporttitornequefermentumnasceturiaculisornare,senectusaliquam
+    dapibusduisposuerevel.Fermentumpermattisduissociosquaptentauctorsodalesaliquamdui,musparturient
+    ornarefeugiatidmaurisnibhinterdumpenatibus,egetsagittisfaucibussednoncumnullafusce.Odiosedsagittis
+    sociosqutemporpotentiinterdum,nequeridiculussempercondimentumlaoreettristiquemolestie,enimmetusmollis
+    inceptosluctus.Nostralacusetduiperfacilisihacornare,quameuismodarcudiamnullamposuereenim,velit
+    auguequisquemusnechabitant.</p>
+<p>Montesanislproindiammaurismagnisinceptossagittis,orciblanditligulacumnequepharetragravida,tellusquam
+    pulvinarnetuspraesentinsociosqu.Magnaodioanteethac,felismorbidignissimdiamdonec,ullamcorperquisque
+    posuere.Vehiculaleosemperpotentisenectusmolestiepretiumfringillanostra,etiamornaresuspendissejustonisi
+    mollisnislliberoquis,hendreritelementumconguearcusedmaecenasneque.Disproinaccumsanprimispellentesque
+    torquentvariusnequeestrhoncus,vehiculaconubiamassametusarculaciniaadnullameuismod,maurisnibhnascetur
+    aliquammolestiefuscetellusmus.Dismattisduisposueremagnisvestibulumad,acfermentumquamegetfacilisis,
+    fusceplaceratclassaeneanblandit.</p>
+<p>Arcunamphasellusliberoeumollisac,tinciduntfelishabitantnislvenenatispretium,interdummolestielacus
+    sapienquam.Aeneanquisqueligulasapienurnafacilisisfeliscrasvehiculatincidunt,elementumenimhimenaeoset
+    auctoranteviverraegetnostra,necrisusfameshacdisdignissimsedclass.Interdumauctorquisvelmassaiaculis
+    justofacilisi,mattismolliscurabiturlobortisclasshabitantmontes,nequeerataccumsandignissimetnulla.Nisi
+    phasellusfacilisisnasceturorcipharetranostraidurnatorquent,blanditvolutpataconguetortorrisusultrices.
+    Proindictumstvenenatisnecmonteshimenaeossociosqusollicitudinnullammauris,curaefamesetinceptosblandit
+    dignissimantearcu.Molestieimperdietegestasnamaccumsanpretiumsemperhacplateamontesvivamuscommodo,
+    fermentumtinciduntcubiliavehiculanullamtristiquenibhnuncaugue.Maecenasvariusconguecuraenonullamcorper
+    acposuereridiculus,pharetralectusegetnascetursuspendissedisnostraauguevenenatis,tinciduntetsuscipit
+    placerategestasdictumenim.Aaliquetrhoncustemporpulvinarsagittissapienhabitassemattislaoreetcras
+    pharetraproinodio,venenatistempusaliquamsociisactellusphasellusauctorvitaefelisaenean.Magnisvulputate
+    sagittismollisultriciesproinetfacilisis,nampraesentiddoneccondimentumposuere.Acpretiumiaculismontes
+    facilisiodio,muspulvinarerossollicitudin,litoramassasuscipitnisi.Tristiquejustoantehendreritmorbiquam
+    interdumdictumstquis,habitantvelmassanonetiamenimtaciti,praesentauctoracridiculusplaceratvolutpat
+    eros.Proinpretiumhendreritsuspendissejustohabitantimperdietcraslaoreet,aeneanadfringillaaccumsan
+    porttitorgravidafermentum,quisnislsenectusatultriciesluctusnunc.</p>
+<p>Cubilialigulaintegermauristorquentportamalesuada,diamarcuelementumvitaealiquam,magnarisustincidunt
+    gravidaconubia.Natoquerisusfusceestpotentieratultriciesetiam,fermentumfacilisisbibenduminmihabitant
+    lacinia,lacuseleifendvulputateutdapibushac.Sodalesmagnisegetpotentiultricesegestasrhoncuscondimentum
+    platea,vitaeorcieleifendnibhvelsociosquhimenaeos,tristiquepulvinarscelerisqueauguetacitifacilisis
+    ornare.Facilisinisilitoraligulaparturientnecinterdum,facilisismusrisusfelisinceptos,curabiturpulvinar
+    idcubiliaconubia.Adidfringillahabitassequisquenibhantenetusporttitornamultriciesdapibusullamcorper,
+    eratmaecenasluctusquishabitantplateafermentummilacuspurusmagnis,nullaarcuparturienttortorcurabitur
+    pharetraorciinceptostempornostrasapien.</p>
+</body>
+</html>
